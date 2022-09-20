875b19610d2235e66f629bc6e929b3e2a05f9677
hyeonsoo <avt5560@gmail.com>
2022-09-21 03:56:03 +0900

    -틀-

--- app/src/main/java/com/cookandroid/loarang/InfoFragment.java
+++ app/src/main/java/com/cookandroid/loarang/InfoFragment.java
@@ -12,51 +12,32 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class InfoFragment extends Fragment {
-    WebView info_web;
+
+    TextView goldCal;TextView personNum;TextView minGold; TextView recomGold; TextView maxGold;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_info, container, false);
-        info_web = view.findViewById(R.id.info_web);
 
-        /* 이미지 정책 변경 후 조치
-        if(Build.VERSION.SDK_INT >= 21) {
-        sex
-            info_web.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        }
-        */
-        info_web.getSettings().setJavaScriptEnabled(true);
-        info_web.setWebViewClient(new infoWebViewClient());
-        WebSettings webSet = info_web.getSettings();
-        webSet.setBuiltInZoomControls(true);
-        info_web.loadUrl("https://lostark.inven.co.kr/dataninfo/world/");
-        info_web.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                if(keyEvent.getAction() == keyEvent.ACTION_DOWN){
-                    if(i == keyEvent.KEYCODE_BACK) {
-                        if(info_web!=null) {
-                            if(info_web.canGoBack()) {
-                                info_web.goBack();
-                            }
-                            else {
-                                getActivity().onBackPressed();
-                            }
-                        }
-                    }
-                }
-                return true;
-            }
-        });
+        goldCal = (TextView) view.findViewById(R.id.GoldCal);
+        personNum = (TextView) view.findViewById(R.id.PersonNum);
+        minGold = (TextView) view.findViewById(R.id.minGold);
+        recomGold = (TextView) view.findViewById(R.id.recomGold);
+        maxGold = (TextView) view.findViewById(R.id.maxGold);
+
         return view;
     }
 
-    class infoWebViewClient extends WebViewClient {
+    /*class infoWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             return super.shouldOverrideUrlLoading(view, url);
         }
-    }
+    }*/
 }
