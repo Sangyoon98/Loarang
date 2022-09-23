4c2a6383e499a282949900eb68269b2ded36f25c
sangyoon <qlrqod123123@naver.com>
2022-09-24 02:28:04 +0900

    정보실 탭 구현

--- app/src/main/java/com/cookandroid/loarang/InfoFragment.java
+++ app/src/main/java/com/cookandroid/loarang/InfoFragment.java
@@ -1,24 +1,16 @@
 package com.cookandroid.loarang;
 
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class InfoFragment extends Fragment {
 
